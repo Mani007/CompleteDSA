f35a75ee01417cc4cdd62bc1d9087fb03775889f
Mohnish <mohnish.a007@gmail.com>
2024-12-31 00:15:17 +0000

    basics of pointer complete including format specifiers in C

--- C/Basic DS/pointers/pointers.c
+++ C/Basic DS/pointers/pointers.c
@@ -18,6 +18,10 @@ int main() {
     
     // Print the value of num
     printf("Value of num: %d\n", num);
+    // Print the value of *ptr
+    printf("Value of num: %x\n", ptr);
+    // Print the value of num using *ptr, make sure you use correct %d not %x also known as format specifiers
+    printf("Value of num: %d\n", *ptr);
     
     return 0;
 }
