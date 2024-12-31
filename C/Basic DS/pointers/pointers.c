444b5db6c9bdba0a6c8db32977224435c5078247
Mohnish <mohnish.a007@gmail.com>
2024-12-31 00:19:59 +0000

    basics of pointer to array in bad way in C

--- C/Basic DS/pointers/pointers.c
+++ C/Basic DS/pointers/pointers.c
@@ -4,22 +4,22 @@
 #include <stdio.h>
 
 int main() {
-    // Declare and initialize an integer variable
-    int num = 10;
+    // Declare and initialize an integer array
+    int num[5] ;
     
     // Declare a pointer to an integer variable
     int *ptr;
     
     // Assign the address of num to ptr
-    ptr = &num;
+    ptr = num;
     
     // Modify the value pointed by ptr
-    *ptr = 20;
+    //*ptr = 20;
     
     // Print the value of num
     printf("Value of num: %d\n", num);
     // Print the value of *ptr
-    printf("Value of num: %x\n", ptr);
+    printf("Value of num: %p\n", ptr);
     // Print the value of num using *ptr, make sure you use correct %d not %x also known as format specifiers
     printf("Value of num: %d\n", *ptr);
     
